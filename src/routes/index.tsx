1bf509ba6a87c33b141c0f6d51759a77b1a696c6
Thomas-Conrad1 <147281837+Thomas-Conrad1@users.noreply.github.com>
2026-09-25 16:59:20 -0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import photo from "@/assets/thomas-conrad.jpeg.asset.json";
+import photo from "@/assets/thomas-conrad.jpeg";
 
 export const Route = createFileRoute("/")({
   head: () => ({
